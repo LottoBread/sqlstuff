61b37e14a932ae73591eb53c64db44f249a86224
LottoBread <stringzbeats@gmail.com>
2023-01-23 14:12:53 -0800

    fixed things and broke other things

--- sql.cpp
+++ sql.cpp
@@ -73,20 +73,21 @@ auto execSQLQuery(MYSQL *connection, string query)
         success = false;
     }
 
-    return result{success, mysql_use_result(connection)};
+    return result{success, mysql_store_result(connection)};
 }
 
 
 void dbUpdate(MYSQL* con, string column, string username, string input){
-    execSQLQuery(con, "UPDATE usr\n SET " + column +  "= \"" + input + "\"\n WHERE username = \"" + username + "\";" );
+    auto result = execSQLQuery(con, "UPDATE usr\n SET " + column +  "= \"" + input + "\"\n WHERE username = \"" + username + "\";" );
+    mysql_free_result(result.res);
 }
 
-auto dbPull(MYSQL* con, string column, string username){
-    auto result = execSQLQuery(con, "SELECT " + column + " FROM usr WHERE username = \"" + username + "\";"); //\"" + username + "\";");
+auto dbPull(MYSQL* con, string column, string username){ //when getting a null result from DB it causes segmentation fault
+    auto result = execSQLQuery(con, "SELECT " + column + " FROM usr WHERE username = \"" + username + "\";");
     MYSQL_ROW row;
     row = mysql_fetch_row(result.res);
-
-    return row[0];
+    mysql_free_result(result.res);
+    return row[0];        
 }
 
 void dbRegister(MYSQL* con, string username, string password){
@@ -96,10 +97,11 @@ void dbRegister(MYSQL* con, string username, string password){
 
 int main(int argc, char const *argv[])
 {
+
     bool success;
     string newstring;
+    bool menu = true;
     
-
     MYSQL *con; // the connection
     // MYSQL_RES *res; // the results
     MYSQL_ROW row; // the results rows (array)
@@ -109,59 +111,63 @@ int main(int argc, char const *argv[])
     // connect to the mysql database
     std::tie(success, con) = sqlConnectionSetup(sqlDetails);
 
-    
-
     if (!success)
     {
         // you can handle any errors here
         return 1;
     }
-    while(login != true){
+    while(menu == true){
         int choice;
         cout << "1) Register a new account.\n2) Log in to an existing account.\n3) Exit the program.";
         cin >> choice;
             switch(choice){
                 case 1:{
 
-                    
                     cout << "Register a new account\n";
                     cout << "Enter a username\n";
                     cin >> username;
-                        if(dbPull(con, "username", username) == username){
+                        if(dbPull(con, "username", username) == username){ //username already exists works, when user check returns null, program throws segmentation fault.
                             cout << "User already exists.\n";
                     }
-                        else if(dbPull(con, "username", username) != username){
+                        else{
                             cout << "Enter a password\n";
                             cin >> password;
                             dbRegister(con, username, password);
                             cout << "Account registered, given default balance of 10,000\n";
+                            menu = false;
                     }
-                    
-                }
                     break;
+                }
+                    
                 case 2:{
                     cout << "Type a username:\n";
                     cin >> username;
                     cout << "Enter password\n";
+                    cout << dbPull(con, "username", username);
                     cin >> newstring;
                     auto fetched = dbPull(con, "password", username);
                     if(fetched == newstring)
                     {
                         cout << "Signed in\n";
                         login = true;
+                        menu = false;
                     }
                     else if (fetched != newstring)
                     {
                         cout << "Password does not match\n";
                         login = false;
+                        menu = false;
                     }
                     break;
                 }
                 case 3:{
-                    playing = true;
+                    menu = false;
                     break;
                 }
             
+            }
+            if(login == true){
+                cout << "Your Balance is: " << dbPull(con, "balance", username);
             }
         }
 
@@ -176,14 +182,14 @@ int main(int argc, char const *argv[])
 
     //if (!result.success)
     //{
-        // handle any errors
+    //     handle any errors
     //    return 1;
     //}
 
     //cout << ("Database Output:\n") << std::endl;
 
 
-    // clean up the database result
+     //clean up the database result
     //mysql_free_result(result.res);
 
-    // close database connection
+     //close database connection
